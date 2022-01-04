21313d484d693b4f8c53c99c7644bbf831698055
aryanjain9953 <aryanj@digitizeco.in>
2022-01-04 19:55:26 +0530

    --

--- src/api/firebaseAPI.js
+++ src/api/firebaseAPI.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 //Paste your firebase configuration here
 var config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyBFtOXiKxBoJ4kBOPeLzdnAvWM31TbSAPA",
+  authDomain: "digitize-52e01.firebaseapp.com",
+  databaseURL: "https://digitize-52e01.firebaseio.com",
+  projectId: "digitize-52e01",
+  storageBucket: "digitize-52e01.appspot.com",
+  messagingSenderId: "649747799992"
 };
 
 
